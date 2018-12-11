7d34d83b8bd6441b5bd7bb6d982a00e1c35540e4
tuanpham <pham.anh.tuan@framgia.com>
2018-12-11 14:58:11 +0700

    user with devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require rails-ujs
+//= require jquery
+//= require_tree .
 //= require bootstrap-sprockets
 //= require activestorage
-//= require_tree .
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :user_confirmed, :authenticate_user!
+  
   def index
   end
 end
