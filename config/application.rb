e3f873924e7b9f7e96e42fa1b787af01d7bd0a89
tuanpham <pham.anh.tuan@framgia.com>
2018-12-13 17:00:37 +0700

    add page default

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module ProjectEnd
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
+    config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
