686bba456e4f15b77fed0ad7bff1bbf036ca94ed
tuanpham <pham.anh.tuan@framgia.com>
2019-01-14 08:30:29 +0700

    change db 2

---
+++ db/migrate/20190114012846_add_usercode_to_user.rb
@@ -0,0 +1,5 @@
+class AddUsercodeToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :usercode, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_041417) do
+ActiveRecord::Schema.define(version: 2019_01_14_012846) do
 
   create_table "attendances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "day_check", null: false
@@ -256,6 +256,7 @@ ActiveRecord::Schema.define(version: 2019_01_10_041417) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.boolean "working", default: true
+    t.string "usercode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
