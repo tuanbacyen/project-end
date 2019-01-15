9dac02cffefbea17c3f72f4590a4bae61e3f12b4
tuanpham <pham.anh.tuan@framgia.com>
2019-01-15 16:07:52 +0700

    convention rubocop 2

--- app/controllers/student_subjects_controller.rb
+++ app/controllers/student_subjects_controller.rb
@@ -4,24 +4,19 @@ class StudentSubjectsController < ApplicationController
   before_action :get_student, only: [:index, :new, :create, :destroy]
   before_action :get_result, only: :show_result
 
-  def show
-  end
+  def show; end
 
-  def show_result
-  end
+  def show_result; end
 
   def index
     @student_classrooms = @student.student_classrooms
   end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 
   private
 
--- app/helpers/student_subjects_helper.rb
+++ app/helpers/student_subjects_helper.rb
@@ -5,17 +5,18 @@ module StudentSubjectsHelper
   end
 
   def score_to_text student_classroom
-    return "chưa có điểm" unless student_classroom.student_subjects.present?
-    score = student_classroom.student_subjects.average(:average).to_f.round(2)
-    if score < 3.5
+    score = average_of_student_in_class student_classroom
+    return score if score.is_a? String
+    case score
+    when 0..3.5
       "Đúp"
-    elsif score >= 3.5 && score < 5
+    when 3.5..5
       "Yếu"
-    elsif score >= 5 && score < 6.5
+    when 5..6.5
       "Trung bình"
-    elsif score >= 6.5 && score < 8
+    when 6.5..8
       "Khá"
-    elsif score >= 8
+    else
       "Giỏi"
     end
   end
