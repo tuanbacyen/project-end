1809a594ecf39d64eada68d8fc11e4978ba05322
tuanpham <pham.anh.tuan@framgia.com>
2019-01-17 16:42:55 +0700

    new design point

--- app/models/point.rb
+++ app/models/point.rb
@@ -5,4 +5,29 @@ class Point < ApplicationRecord
   delegate :name, to: :point_type, prefix: true, allow_nil: true
 
   validates :point, presence: true, inclusion: {in: 0..10, message: "%{value} is not a valid size"}
+
+  after_create :update_student_subject
+  after_update :update_student_subject
+  after_destroy :update_student_subject
+
+  scope :load_point_mouth, (lambda do
+    where(point_type_id: PointType.point_mouth.id)
+  end)
+
+  scope :load_point_fifteen, (lambda do
+    where(point_type_id: PointType.point_fifteen.id)
+  end)
+
+  scope :load_point_sixty, (lambda do
+    where(point_type_id: PointType.point_sixty.id)
+  end)
+
+  scope :load_point_finish, (lambda do
+    where(point_type_id: PointType.point_finish.id)
+  end)
+
+  private
+  def update_student_subject
+    StudentSubject.find_by(id: student_subject_id).update_average
+  end
 end
