37208ed7d873fb2ebdc002f7b3e477335868f3ea
tuanpham <pham.anh.tuan@framgia.com>
2019-01-29 12:50:19 +0700

    manage class auto create subject

--- app/assets/javascripts/semesters.js
+++ app/assets/javascripts/semesters.js
@@ -16,23 +16,72 @@ $(document).ready(function() {
 
   $('body').on('click', '.create-class', function(){
     var tr = $(this).closest('tr');
-    var data = {
-      auto_create: tr.find('#auto_subject')[0].checked,
-      classroom: {
-        semester_id: $('#semester_hidden').text(),
-        user_id: tr.find('.select_teacher').val(),
-        number: tr.find('#select_num').val(),
-        name: tr.find('#name_class').val()
-      }
-    };
-    send_ajax_new_semester('POST', '/new_semesters', data, null, $(this).closest('tr'));
+    if(check_class_name_lenght(tr.find('#name_class').val())){
+      alert('Tên lớp dài quá 10 ký tự');
+    } else if(check_teacher_exist(tr.find('.select_teacher').val())){
+      alert('Giáo viên đã là chủ nhiệm');
+    } else if(check_class_name_exist(tr.find('#name_class').val())) {
+      alert('Tên lớp học đã tồn tại trong học kỳ')
+    }
+    else {
+      var data = {
+        auto_create: tr.find('#auto_subject')[0].checked,
+        classroom: {
+          semester_id: $('#semester_hidden').text(),
+          user_id: tr.find('.select_teacher').val(),
+          number: tr.find('#select_num').val(),
+          name: tr.find('#name_class').val()
+        }
+      };
+      send_ajax_new_semester('POST', '/new_semesters', data, null, $(this).closest('tr'));
+    }
   });
 
   $('body').on('click', '.destroy-class', function(){
     $(this).closest('tr').remove();
     show_update_all();
   });
+
+  $('body').on('change', '#select_num', function(){
+    var tr = $(this).closest('tr');
+    tr.find('#name_class').val($("option:selected", $(this)).text());
+  });
 });
+
+function check_teacher_exist(teacher_id){
+  var count = 0;
+  $('td[data-tid]').each(function(){
+    if($(this).data('tid').toString() == teacher_id.toString()){
+      count++;
+    }
+  });
+  if (count > 0)
+    return true;
+  else
+    return false;
+}
+
+function check_class_name_lenght(name){
+  if(name.length > 10)
+    return true;
+  else
+    return false;
+}
+
+function check_class_name_exist(name){
+  var count = 0;
+  $('td[id="class_name"]').each(function(){
+    var class_name = this.textContent;
+    if(class_name == "")
+      class_name = $(this).find('#name_class').val();
+    if(class_name == name)
+      count++;
+  });
+  if(count > 1)
+    return true;
+  else
+    return false;
+}
 
 function show_update_all(){
   var btn_update = '<button class="btn btn-info pull-right" id="update_all"><i class="fa fa-save"></i>Update all</button>';
@@ -69,9 +118,10 @@ function get_count_max() {
 function render_tr(count_max){
   return '<tr><td id="index_class">'+ count_max +'</td>\
           <td>'+ select_num() +'</td>\
-          <td><input class="form-control" type="text" value="Lớp 1" id="name_class"></td>\
+          <td id="class_name"><input class="form-control" type="text" value="Lớp 1" id="name_class"></td>\
           <td><select class="chosen-select select_teacher" id="select_teacher_'+ count_max +'"></select></td>\
-          <td><input type="checkbox" class="icheck-me" name="auto" id="auto_subject" checked data-skin="square" data-color="blue"><br></td>\
+          <td><input type="checkbox" class="icheck-me" name="auto" id="auto_subject" checked data-skin="square" data-color="blue">\
+          <strong> auto create subject</strong></td>\
           <td><button class="btn btn-success create-class">\
           <i class="fa fa-save"></i></button>\
           <button class="btn btn-default destroy-class">\
@@ -82,8 +132,8 @@ function render_tr_result(tr){
   var select_teacher = "#select_teacher_" + tr.find('#index_class').text();
   return '<tr><td id="index_class">'+ tr.find('#index_class').text() +'</td>\
           <td>'+ tr.find('#select_num option:selected').text() +'</td>\
-          <td>'+ tr.find('#name_class').val() +'</td>\
-          <td>'+ tr.find(select_teacher + ' option:selected').text() +'</td>\
+          <td id="class_name">'+ tr.find('#name_class').val() +'</td>\
+          <td data-tid="'+ tr.find('.select_teacher').val() +'">'+ tr.find(select_teacher + ' option:selected').text() +'</td>\
           <td>0</td><td>0</td></tr>';
 }
 
@@ -97,7 +147,7 @@ function send_ajax_new_semester(method, url, data, id, tr_result){
       if(id == null){
         if(data['notifi'].type == "1"){
           var tbl = $('#tbl_class').find('tbody');
-          tbl.append(render_tr_result(tr_result));          
+          tbl.append(render_tr_result(tr_result));
           tr_result.remove();
           show_update_all();
         } else {
@@ -120,7 +170,7 @@ function callback_select(data, id){
   select.empty();
   if(data.list_teacher.length > 0){
     for(var i = 0; i < data.list_teacher.length; i++){
-      select.append(`<option value="${data.list_teacher[i][1]}">${data.list_teacher[i][0]}</option>`); 
+      select.append(`<option value="${data.list_teacher[i][1]}">${data.list_teacher[i][0]}</option>`);
     }
   }
   select.trigger("chosen:updated");
